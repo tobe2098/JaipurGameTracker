3c2c7f702f173892f95ab2be8e29148752ba7dba
Antoni Bertolin <tonibm1220@gmail.com>
2024-09-14 12:43:51 +0200

    Create Jaipur

---
+++ Jaipur/src/game_constants.h
@@ -0,0 +1,23 @@
+#ifndef GAME_CONSTANTS_JAIPUR
+#define GAME_CONSTANTS_JAIPUR
+
+const int diamond_tokens[] = { 7, 7, 5, 5, 5 };
+const int diamond_t_size   = 5;
+const int gold_tokens[]    = { 6, 6, 5, 5, 5 };
+const int gold_t_size      = 5;
+const int silver_tokens[]  = { 5, 5, 5, 5, 5 };
+const int silver_t_size    = 5;
+
+const int spice_tokens[]   = { 5, 3, 3, 2, 2, 1, 1 };
+const int spice_t_size     = 7;
+const int cloth_tokens[]   = { 5, 3, 3, 2, 2, 1, 1 };
+const int cloth_t_size     = 7;
+const int leather_tokens[] = { 4, 3, 2, 1, 1, 1, 1, 1, 1 };
+const int leather_t_size   = 9;
+
+const int bonus_template_3[] = { 3, 3, 2, 2, 1, 1 };
+const int bonus_template_4[] = { 6, 6, 5, 5, 4, 4 };
+const int bonus_template_5[] = { 9, 9, 8, 8, 7, 7 };
+
+const int camel_token = 5;
+#endif
